03ef7bda3a1b00bd1c3ade887c7a8b835cf8f791
Ariel Mendoza <ariel.azem@gmail.com>
2020-10-22 01:29:33 -0700

    Added web instances

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -94,6 +94,46 @@ resource "aws_security_group" "nginx" {
   }
 }
 
+resource "aws_security_group" "web" {
+  name   = "${var.prefix}-web"
+  vpc_id = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 8300
+    to_port     = 8300
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "consul" {
   name   = "${var.prefix}-consul"
   vpc_id = "${module.vpc.vpc_id}"
