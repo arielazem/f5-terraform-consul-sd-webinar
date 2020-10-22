a1a6d4b06b6959c4ab0668717c610fa245768357
Ariel Mendoza <ariel.azem@gmail.com>
2020-10-21 23:58:09 -0700

    Logic to install consul-tf-sync

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -66,3 +66,71 @@ EOF
 sudo systemctl enable consul
 sudo service consul start
 sudo service consul status
+
+################  Consul-Terraform-Sync  ###########################
+
+#Download Consul-Terraform-Sync
+CONSUL_TF_SYNC_VERSION="0.1.0-techpreview1"
+curl --silent --remote-name https://releases.hashicorp.com/consul-terraform-sync/${CONSUL_TF_SYNC_VERSION}/consul-terraform-sync_${CONSUL_TF_SYNC_VERSION}_linux_amd64.zip
+
+#Install Consul-Terraform-Sync
+sudo apt install unzip
+unzip consul-terraform-sync_${CONSUL_TF_SYNC_VERSION}_linux_amd64.zip
+sudo chown root:root consul-terraform-sync
+sudo mv consul-terraform-sync /usr/local/bin/
+
+#Create config dir
+sudo mkdir --parents /etc/consul-tf-sync.d
+sudo touch /etc/consul-tf-sync.d/consul-tf-sync.hcl
+sudo chown --recursive consul:consul /etc/consul-tf-sync.d
+sudo chmod 640 /etc/consul-tf-sync.d/consul-tf-sync.hcl
+
+cat << EOF > /tmp/consul-tf-sync.hcl
+driver "terraform" {
+  log = true
+  required_providers {
+    bigip = {
+      source = "F5Networks/bigip"
+    }
+  }
+}
+consul {
+  address = "0.0.0.0:8500"
+}
+
+provider "bigip" {
+  address  = ""
+  username = ""
+  password = ""
+}
+
+task {
+  name = "AS3"
+  description = "Create AS3 Applications"
+  source = "arielazem/app-consul-sync-nia/bigip"
+  providers = ["bigip"]
+  services = ["nginx"]
+}
+EOF
+sudo cp /tmp/consul-tf-sync.hcl /etc/consul-tf-sync.d/consul-tf-sync.hcl
+
+#Create Systemd Config
+cat << EOF > /tmp/consul-tf-sync.service
+[Unit]
+Description="HashiCorp Consul Terraform Sync"
+Requires=network-online.target
+After=network-online.target
+ConditionFileNotEmpty=/etc/consul-tf-sync.d/consul-tf-sync.hcl
+
+[Service]
+User=consul
+Group=consul
+ExecStart=/usr/local/bin/consul-terraform-sync -config-dir /etc/consul-tf-sync.d/
+KillMode=process
+Restart=always
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp /tmp/consul-tf-sync.service /etc/systemd/system/consul-tf-sync.service
